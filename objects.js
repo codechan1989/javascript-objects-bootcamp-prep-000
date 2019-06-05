df9f6e0b32b8ed0cff27457b9eaba9c95ad42155
Sho Ishikura <shoishikura@gmail.com>
2019-06-05 14:57:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({artistName: 'songTitle'})
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
